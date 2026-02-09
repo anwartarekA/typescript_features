d14213bc74c38d616f7a273b00b84e4155fb22dc
= <=>
2026-02-09 03:10:11 +0000

    features of ts

---
+++ annotations/variables.ts
@@ -0,0 +1,17 @@
+let user: string = 'anwar';
+let num: number = 10;
+let has: boolean = true;
+let nothingMatch: null = null;
+let nothing: undefined = undefined;
+let now: Date = new Date();
+now.getMonth();
+class Color {}
+const red: Color = new Color();
+let strings: string[] = ['red', 'green', 'blue'];
+let numbers: number[] = [1, 2, 3];
+let booleans: boolean[] = [true, true, false];
+let position: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
+console.log(strings);
