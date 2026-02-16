8dd82be5a953b2b02a229410171b13a915f770cf
= <=>
2026-02-16 00:33:00 +0000

    annotations around arrays

---
+++ arrays.ts
@@ -0,0 +1,6 @@
+const carMaker = ['ford', 'toyota', 'fia'];
+const car: string[] = [];
+const dates = [new Date(), new Date()];
+const dates2: Date[] = [new Date(), new Date()];
+const canMakeBy = [['fo12'], ['toyo12']];
+const arr: string[][] = [];
