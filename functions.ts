d6cab3621f387e13d3a18a449881294cf7914ee8
= <=>
2026-02-16 00:05:02 +0000

    inference aroundfunctions

--- functions.ts
+++ functions.ts
@@ -3,3 +3,9 @@ const add = (a: number, b: number): number => {
   return a + b;
 };
 console.log(add(10, 30));
+
+// type infernce around function
+const subtract = (a: number, b: number) => {
+  return a - b;
+};
+console.log(subtract(10, 5));
