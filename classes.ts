d90d175919b18fd70a2153a54b6acae41a624fab
= <=>
2026-02-25 02:14:52 +0000

    classes and modifiers

--- classes.ts
+++ classes.ts
@@ -1,13 +1,21 @@
 class Vehicle {
-  drive(): void {
-    console.log('chugga chugga');
+  protected driveCar(): void {
+    console.log('hahahahah');
   }
 }
+
 class Car extends Vehicle {
+  private drive(): void {
+    console.log('vroom vroom');
+  }
   honk(): void {
     console.log('beep beep');
   }
+  drivingProcess(): void {
+    this.drive();
+    this.honk();
+    this.driveCar();
+  }
 }
 const car = new Car();
-car.drive();
-car.honk();
+car.drivingProcess();
